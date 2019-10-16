6f69beccb5009aaeadd5b9c622d1b8781d35f8ee
KimVy Nguyen <kimvy.ngu@gmail.com>
2019-10-16 12:34:26 -0400

    Lab 21

---
+++ Models/Fish.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApp.Models
+{
+    public class Fish
+    {
+        public string Name { get; set; }
+        public int Scales { get; set; }
+        public string Species { get; set; }
+        public bool IsFreshWater { get; set; }
+
+        public Fish()
+        {
+
+        }
+
+        public Fish(string Name, int Scales, string Species, bool IsFreshWater)
+        {
+            this.Name = Name;
+            this.Scales = Scales;
+            this.Species = Species;
+            this.IsFreshWater = IsFreshWater;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApp.Models
+{
+    public class User
+    {
+        [Key]
+        [Required(ErrorMessage = "Please enter a user name")]
+        [StringLength(10, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(10)]
+        [Required]
+        public string Password { get; set; } 
+
+        //add in name email phone number etc
+
+        [Range(18, 99)]
+        public int Age { get; set; }
+    }
+
+   
+}
